d62fa7667fd9ede0e2850a7bd15efd2dd2291b87
Sabine Robart <sabine.robart@protonmail.com>
2019-03-17 19:26:39 +0100

    Add article page on Wenov project, plus claps-counter JS file and sound file.

---
+++ claps-counter.js
@@ -0,0 +1,50 @@
+document.body.onload = () => {
+  const rootElement = document.getElementsByClassName('clap-button-box');
+
+  for (let i = 0; i < rootElement.length; i++) {
+    // Create button
+    const newButton = document.createElement("button");
+    newButton.classList.add("claps-button");
+    newButton.addEventListener('click', () => {
+      clickEvents();
+    })
+
+    // Insert clap icon in button
+    const clapsIcon = document.createElement("img");
+    clapsIcon.src = "https://img.icons8.com/ios/60/000000/applause.png";
+    newButton.append(clapsIcon);
+
+    // Append button to elements with class name 'clap-button-box'
+    const eachClapButton = rootElement[i];
+    eachClapButton.append(newButton);
+
+    // Create counter indication
+    const clapsCounter = document.createElement("div");
+    clapsCounter.id = 'clicks';
+    newButton.append(clapsCounter);
+    clapsCounter.classList.add("number");
+    clapsCounter.innerHTML = "533";
+  }
+}
+
+// Create counter incrementation
+let clicks = 533;
+
+const addClaps = () => {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+  let element = document.getElementById("clicks");
+  element.classList.add("animation");
+};
+
+// Clap sound event
+const myPlay = () => {
+  let audio = new Audio("clap.mp3");
+  audio.play();
+}
+
+// Helper function
+const clickEvents = () => {
+  addClaps();
+  myPlay();
+}
